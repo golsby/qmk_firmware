dffbfbba480a15e7dc4aaa639505f62affb8d467
Brian Gillespie <brian@mcneel.com>
2024-07-07 12:53:59 -0700

    Move layers to layers.h Simplify lighting setup Simplify key layouts. Add persistent _LOWER and _RAISE modes

--- keyboards/keebio/iris_ce/keymaps/brian-mac/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/brian-mac/keymap.c
@@ -1,20 +1,9 @@
 // Copyright 2023 Danny Nguyen (@nooges)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include "layers.h"
 #include QMK_KEYBOARD_H
 #define HOMEROW_MODS
-
-
-enum custom_layers {
-    _MAC,
-    _WIN,
-    _RHINO,
-    _LOWER,
-    _WIN_LOWER,
-    _RAISE,
-    _MOUSE,
-};
-
 
 // Tap Dance declarationsp
 enum {
@@ -88,28 +77,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(  //  _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, LGUI(LSFT(LCTL(KC_4))),
+     XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR,  XXXXXXX,                             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, LGUI(LSFT(LCTL(KC_4))),
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-LGUI(KC_TAB),  _______, _______,  KC_LPRN, KC_RPRN, _______,                            KC_BSPC, KC_HOME,   KC_UP,   KC_PGUP,   KC_P0,   _______,
+LGUI(KC_TAB),  XXXXXXX, XXXXXXX,  KC_LPRN, KC_RPRN, XXXXXXX,                            KC_BSPC, KC_HOME,   KC_UP,   KC_PGUP,   KC_P0,   _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-_______,LALT_T(_______), LCTL_T(_______),LGUI_T(KC_LBRC),LSFT_T(KC_RBRC), _______, LGUI(KC_LEFT),RSFT_T(KC_LEFT), LGUI_T(KC_DOWN),RCTL_T(KC_RIGHT),LGUI(KC_RIGHT), LGUI(KC_RIGHT),
+XXXXXXX,LALT_T(XXXXXXX), LCTL_T(XXXXXXX),LGUI_T(KC_LBRC),LSFT_T(KC_RBRC), _______, LGUI(KC_LEFT),KC_LEFT, XXXXXXX,KC_RIGHT,LGUI(KC_RIGHT), LGUI(KC_RIGHT),
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______,  _______, KC_LCBR, KC_RCBR, _______, KC_ESC,          KC_BSPC, XXXXXXX, KC_END,   _______,   KC_PGDN,   KC_BSLS, XXXXXXX,
+   TG(_LOWER),LCMD(KC_Z),LCMD(KC_X),LCMD(KC_C),LCMD(KC_V),MO(_FUNC),KC_ESC,       KC_BSPC, XXXXXXX, KC_END,   KC_DOWN,   KC_PGDN,   KC_BSLS, TG(_LOWER),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LGUI, _______, KC_SPC,                    KC_ENT,  MO(_MOUSE), KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
 
-  [_WIN_LOWER] = LAYOUT(  //  _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER    _LOWER
+  [_WIN_LOWER] = LAYOUT(  //  _WIN_LOWER    _WIN_LOWER    _WIN_LOWER    _WIN_LOWER    _WIN_LOWER    _WIN_LOWER    _WIN_LOWER    _WIN_LOWER    _WIN_LOWER
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PSCR,
+     XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR,  XXXXXXX,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PSCR,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-LGUI(KC_TAB),  _______, _______,  KC_LPRN, KC_RPRN, _______,                          KC_BSPC, LCTL(KC_HOME),KC_UP,KC_PGUP, KC_P0,  _______,
+LGUI(KC_TAB),  XXXXXXX, XXXXXXX,  KC_LPRN, KC_RPRN, XXXXXXX,                        KC_BSPC, LCTL(KC_HOME),KC_UP,KC_PGUP,  KC_P0,  _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-_______,LALT_T(_______), LCTL_T(_______),LGUI_T(KC_LBRC),LSFT_T(KC_RBRC), _______, KC_HOME,RSFT_T(KC_LEFT), LGUI_T(KC_DOWN),RCTL_T(KC_RIGHT),KC_END, KC_END,
+XXXXXXX,LALT_T(XXXXXXX), LCTL_T(XXXXXXX),LGUI_T(KC_LBRC),LSFT_T(KC_RBRC),_______,     KC_HOME, KC_LEFT, XXXXXXX, KC_RIGHT, KC_END, KC_END,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______,  _______, KC_LCBR, KC_RCBR, _______, KC_ESC,          KC_BSPC, XXXXXXX, LCTL(KC_END),   _______,   KC_PGDN,   KC_BSLS, XXXXXXX,
+     TG(_LOWER),LCTL(KC_Z),LCTL(KC_X),LCTL(KC_C),LCTL(KC_V), XXXXXXX, KC_ESC,   KC_BSPC, XXXXXXX, LCTL(KC_END),   KC_DOWN,   KC_PGDN,   KC_BSLS, TG(_LOWER),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LGUI, _______, KC_SPC,                    KC_ENT,  MO(_MOUSE), KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -121,26 +110,38 @@ _______,LALT_T(_______), LCTL_T(_______),LGUI_T(KC_LBRC),LSFT_T(KC_RBRC), ______
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_SLASH, KC_BACKSLASH,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_VAI, KC_MPRV, KC_MNXT, KC_VOLU, _______, KC_MINS,                            KC_PLUS,  KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ASTERISK, _______,
+     RGB_VAI, KC_MPRV, KC_MNXT, KC_VOLU, KC_BRIU, KC_MINS,                            KC_PLUS,  KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_ASTERISK, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_VAD, KC_MSTP, KC_MPLY, KC_VOLD, _______, KC_UNDS, KC_ESC,          KC_BSPC,  KC_EQL,  KC_KP_1,  KC_KP_2, KC_KP_3, KC_PDOT, _______,
+     RGB_VAD, KC_MSTP, KC_MPLY, KC_VOLD, KC_BRID, KC_UNDS, KC_ESC,          KC_BSPC,  KC_EQL,  KC_KP_1,  KC_KP_2, KC_KP_3, KC_PDOT, TG(_RAISE),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______,   MO(_MOUSE), KC_SPC,                KC_ENT, _______, KC_KP_0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
 
-
-
  [_MOUSE] = LAYOUT(  //  _MOUSE    _MOUSE    _MOUSE    _MOUSE    _MOUSE    _MOUSE    _MOUSE    _MOUSE    _MOUSE    _MOUSE    _MOUSE    _MOUSE    _MOUSE
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     QK_BOOT, TO(_MAC), TO(_WIN), TO(_RHINO), _______, _______,                            _______, _______, _______, _______, _______, QK_CLEAR_EEPROM,
+     QK_BOOT, TO(_MAC),TO(_WIN),TG(_FUNC),TG(_LOWER), _______,                        _______, _______, _______, _______, _______, QK_CLEAR_EEPROM,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, DM_REC1, DM_PLY1,  _______, _______, _______,                           KC_BTN3,  KC_WH_D, KC_MS_U, _______, _______, _______,
+     RGB_TOG, DM_REC1, DM_PLY1,  _______, _______, _______,                           KC_BTN3,  KC_WH_D, KC_MS_U, XXXXXXX, XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, KC_BTN2, KC_BTN1, _______,                            KC_BTN1,  KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2, _______,
+     _______, _______, _______, KC_BTN2, KC_BTN1, _______,                            KC_BTN1,  KC_MS_L, XXXXXXX, KC_MS_R, KC_BTN2, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, KC_BTN3, _______, _______, _______,          _______, _______, KC_WH_U,  KC_MS_D, _______, _______, _______,
+     TG(_RAISE), _______, _______, KC_BTN3, _______, _______, _______,          _______, _______, KC_WH_U,  KC_MS_D, XXXXXXX, XXXXXXX, TG(_LOWER),
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, TG(_MOUSE), TG(_MOUSE)
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+ [_FUNC] = LAYOUT(  //  _FUNC    _FUNC    _FUNC    _FUNC    _FUNC    _FUNC    _FUNC    _FUNC    _FUNC    _FUNC    _FUNC    _FUNC    _FUNC
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -148,41 +149,6 @@ _______,LALT_T(_______), LCTL_T(_______),LGUI_T(KC_LBRC),LSFT_T(KC_RBRC), ______
 
 };
 
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    //rgblight_sethsv(140,0,255);
-
-    if (layer_state_cmp(state, _MOUSE)) {
-        rgb_matrix_mode(RGB_MATRIX_CUSTOM_mouse_effect);
-    }
-    else if (layer_state_cmp(state, _LOWER)) {
-        //rgb_matrix_mode(RGB_MATRIX_CUSTOM_lower_mac_effect);
-    }
-    else if (layer_state_cmp(state, _WIN_LOWER)) {
-        //rgb_matrix_mode(RGB_MATRIX_CUSTOM_lower_win_effect);
-    }
-    else if (layer_state_cmp(state, _RAISE)) {
-        rgb_matrix_mode(RGB_MATRIX_CUSTOM_raise_effect);
-    }
-    else if (layer_state_cmp(state, _RHINO)) {
-        //rgblight_sethsv_noeeprom(180,120,96); // lavender
-        rgb_matrix_mode(RGB_MATRIX_CUSTOM_rhino_effect);
-    }
-    else if (layer_state_cmp(state, _MAC)) {
-        //rgblight_sethsv_noeeprom(0,0,96);
-        rgb_matrix_mode(RGB_MATRIX_CUSTOM_dynamic_effect);
-    }
-    else if (layer_state_cmp(state, _WIN)) {
-        //rgblight_sethsv_noeeprom(0,80,96);  // red
-        rgb_matrix_mode(RGB_MATRIX_CUSTOM_win_effect);
-    }
-
-    else {
-        rgb_matrix_mode(RGB_MATRIX_CUSTOM_dynamic_effect);
-    }
-
-    return state;
-}
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -201,17 +167,22 @@ void keyboard_post_init_user(void) {
 
 const uint16_t PROGMEM esc_combo[] = {KC_SPACE, KC_ESC, COMBO_END};
 const uint16_t PROGMEM lower_bkspc_combo[] = {KC_BACKSPACE, LGUI(KC_LEFT), COMBO_END};
-const uint16_t PROGMEM lower_bkspc_combo2[] = {KC_HOME, RSFT_T(KC_LEFT), COMBO_END};
+const uint16_t PROGMEM win_lower_bkspc_combo2[] = {LCTL(KC_HOME), KC_LEFT, COMBO_END};
+const uint16_t PROGMEM win_lower_bkspc_combo[] = {KC_BACKSPACE, KC_HOME, COMBO_END};
+const uint16_t PROGMEM lower_bkspc_combo2[] = {KC_HOME, KC_LEFT, COMBO_END};
 const uint16_t PROGMEM bkspc_combo[] = {KC_H, KC_Y, COMBO_END};
 const uint16_t PROGMEM bkspc_combo2[] = {KC_U, RSFT_T(KC_J) , COMBO_END};
-const uint16_t PROGMEM raise_bkspc_combo[] = {KC_4, KC_7, COMBO_END};
+const uint16_t PROGMEM raise_bkspc_combo[] = {KC_KP_7, KC_KP_4, COMBO_END};
 const uint16_t PROGMEM raise_bkspc_combo2[] = {KC_CIRC, KC_PLUS, COMBO_END};
+
 combo_t key_combos[] = {
     COMBO(esc_combo, KC_ESC),
     COMBO(bkspc_combo, KC_BACKSPACE),
     COMBO(bkspc_combo2, KC_BACKSPACE),
     COMBO(lower_bkspc_combo, KC_BACKSPACE),
     COMBO(lower_bkspc_combo2, KC_BACKSPACE),
+    COMBO(win_lower_bkspc_combo, KC_BACKSPACE),
+    COMBO(win_lower_bkspc_combo2, KC_BACKSPACE),
     COMBO(raise_bkspc_combo, KC_BACKSPACE),
     COMBO(raise_bkspc_combo2, KC_BACKSPACE),
 };
---
+++ keyboards/keebio/iris_ce/keymaps/brian-mac/layers.h
@@ -0,0 +1,14 @@
+#pragma once
+
+enum custom_layers {
+    _MAC,
+    _WIN,
+    _RHINO,
+    _LOWER,
+    _WIN_LOWER,
+    _RAISE,
+    _FUNC,
+    _MOUSE,
+    LAST_LAYER = _MAC,
+    FIRST_LAYER = _MOUSE
+} LAYERS;
