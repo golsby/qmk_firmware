bcdeacc393f1556724e705ca5b4cf579049ba441
Brian Gillespie <brian@mcneel.com>
2025-07-05 13:18:12 -0700

    various

--- keyboards/keebio/iris_ce/keymaps/brian-mac/keys_1.h
+++ keyboards/keebio/iris_ce/keymaps/brian-mac/keys_1.h
@@ -34,13 +34,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_WIN] = LAYOUT(  //  _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN    _WIN
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     MO(_NUMPAD),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  MO(_NUMPAD), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    XXXXXXX, XXXXXXX,
+     KC_LGUI,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     MO(_WIN_LOWER), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_ESC,      KC_BSPC, KC_K,  KC_H,    KC_COMM, KC_DOT,  KC_SLSH, MO(_SYMBOLS),
+ MO(_WIN_LOWER),KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,          KC_BSPC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, MO(_SYMBOLS),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LCTL, KC_LSFT, KC_SPC,                    KC_ENT,  KC_RSFT, KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
